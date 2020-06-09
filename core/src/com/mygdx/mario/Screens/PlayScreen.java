cfc16a0236a1178a10b5e70bacb221fd5e2410d7
kmolik <48009091+kmolik@users.noreply.github.com>
2020-06-09 13:49:30 +0200

    implemented moving animations

--- core/src/com/mygdx/mario/Screens/PlayScreen.java
+++ core/src/com/mygdx/mario/Screens/PlayScreen.java
@@ -5,28 +5,25 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.mario.Scenes.Hud;
 import com.mygdx.mario.Sprites.Mario;
 import com.mygdx.mario.SuperMario;
+import com.mygdx.mario.tools.B2WorldCreator;
 
 public class PlayScreen implements Screen {
 
     private SuperMario game;
+    private TextureAtlas atlas;
+
     private OrthographicCamera gameCam;
     private Viewport gamePort;
     private Hud hud;
@@ -43,6 +40,8 @@ public class PlayScreen implements Screen {
     private Mario player;
 
     public PlayScreen(SuperMario game) {
+        atlas = new TextureAtlas("Mario_and_Enemies.pack");
+
         this.game = game;
         gameCam = new OrthographicCamera();
         gamePort = new FitViewport(SuperMario.V_WIDTH / SuperMario.PPM, SuperMario.V_HEIGHT / SuperMario.PPM, gameCam);
@@ -53,70 +52,17 @@ public class PlayScreen implements Screen {
         renderer = new OrthogonalTiledMapRenderer(map, 1 / SuperMario.PPM);
         gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() /2, 0);
 
-       world = new World(new Vector2(0, -10), true);
-       b2dr = new Box2DDebugRenderer();
+        world = new World(new Vector2(0, -10), true);
+        b2dr = new Box2DDebugRenderer();
 
-       BodyDef bdef = new BodyDef();
-       PolygonShape shape = new PolygonShape();
-       FixtureDef fdef = new FixtureDef();
-       Body body;
+        new B2WorldCreator(world, map);
 
-        player = new Mario(world);
+        player = new Mario(world, this);
 
-       // creating ground bodies/fixtures
-       for(MapObject object: map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
-           Rectangle rect = ((RectangleMapObject) object).getRectangle();
+    }
 
-           bdef.type = BodyDef.BodyType.StaticBody;
-           bdef.position.set((rect.getX() + rect.getWidth() / 2) / SuperMario.PPM, (rect.getY() + rect.getHeight() /2) / SuperMario.PPM);
-
-           body = world.createBody(bdef);
-
-           shape.setAsBox(rect.getWidth() /2 / SuperMario.PPM, rect.getHeight() / 2 / SuperMario.PPM);
-           fdef.shape = shape;
-           body.createFixture(fdef);
-       }
-
-        // creating pipe bodies/fixtures
-        for(MapObject object: map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-            bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set((rect.getX() + rect.getWidth() / 2) / SuperMario.PPM, (rect.getY() + rect.getHeight() /2) / SuperMario.PPM);
-
-            body = world.createBody(bdef);
-
-            shape.setAsBox(rect.getWidth() /2 / SuperMario.PPM, rect.getHeight() / 2 / SuperMario.PPM);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-        }
-        // creating brick bodies/fixtures
-        for(MapObject object: map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)) {
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-            bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set((rect.getX() + rect.getWidth() / 2) / SuperMario.PPM, (rect.getY() + rect.getHeight() /2) / SuperMario.PPM);
-
-            body = world.createBody(bdef);
-
-            shape.setAsBox(rect.getWidth() /2 / SuperMario.PPM, rect.getHeight() / 2 / SuperMario.PPM);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-        }
-        // creating coin bodies/fixtures
-        for(MapObject object: map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-            bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set((rect.getX() + rect.getWidth() / 2) / SuperMario.PPM, (rect.getY() + rect.getHeight() /2) / SuperMario.PPM);
-
-            body = world.createBody(bdef);
-
-            shape.setAsBox(rect.getWidth() /2 / SuperMario.PPM, rect.getHeight() / 2 / SuperMario.PPM);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-        }
-
+    public TextureAtlas getAtlas() {
+        return atlas;
     }
 
     @Override
@@ -138,6 +84,8 @@ public class PlayScreen implements Screen {
 
         world.step(1/60f, 6, 2);
 
+        player.update(dt);
+
         gameCam.position.x = player.b2body.getPosition().x;
 
         //updating gameCam with correct coordinates after changes
@@ -157,6 +105,11 @@ public class PlayScreen implements Screen {
 
         //rendering Box2DDebugLines
         b2dr.render(world, gameCam.combined);
+
+        game.batch.setProjectionMatrix(gameCam.combined);
+        game.batch.begin();
+        player.draw(game.batch);
+        game.batch.end();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
@@ -185,6 +138,10 @@ public class PlayScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        map.dispose();
+        renderer.dispose();
+        world.dispose();
+        b2dr.dispose();
+        hud.dispose();
     }
 }
